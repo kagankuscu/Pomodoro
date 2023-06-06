f7f021f5d5a7ef06db174440f32c2150404e4462
kagankuscu <kuscukagan@gmail.com>
2023-06-06 14:15:39 +0300

    Project Created.

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Kagan Kuscu on 6.06.23.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
